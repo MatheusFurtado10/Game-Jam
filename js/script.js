e232077d3702c41bae27eff8e8ec9a6c9e317f5e
fadel-elias <fadel.elias@estudante.ufjf.br>
2025-01-29 19:17:32 -0300

    corrige style

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const pato = document.querySelector('.pato');
 const bolhas = document.querySelector('.bolhas');
 
-// Função para fazer o pato pular
 const jump = () => {
     pato.classList.add('jump');
 
@@ -10,30 +9,24 @@ const jump = () => {
     }, 1500);
 };
 
-// Loop para verificar colisões
 const loop = setInterval(() => {
-    const bolhasPosition = bolhas.offsetLeft; // Posição horizontal das bolhas
-    const patoPosition = parseFloat(window.getComputedStyle(pato).bottom.replace('px', ''));
+    const bolhasPosition = bolhas.offsetLeft;
+    const patoPosition = +window.getComputedStyle(pato).bottom.replace('px', '');
 
-    // Verifica colisão
-    if (bolhasPosition <= 110 + bolhas.offsetWidth && bolhasPosition > 0 && patoPosition < 90) {
-        // Para a animação das bolhas
+    if (bolhasPosition <= 110  && bolhasPosition > 0 && patoPosition < 90  ) {
         bolhas.style.animation = 'none';
         bolhas.style.left = `${bolhasPosition}px`;
 
-        // Para a animação do pato
         pato.style.animation = 'none';
         pato.style.bottom = `${patoPosition}px`;
 
-        // Troca a imagem do pato
-        pato.src = `./imagens/pato-morre.gif?${new Date().getTime()}`;
+        pato.src = './imagens/pato-morre.gif?' + new Date().getTime();
         pato.style.width = '69px';
         pato.style.marginLeft = '50px';
 
-        // Para o loop
         clearInterval(loop);
-    }
+        
+    } 
 }, 10);
 
-// Adiciona o evento de pulo ao pressionar uma tecla
-document.addEventListener('keydown', jump);
+document.addEventListener('keydown', jump);
